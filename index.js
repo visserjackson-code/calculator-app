53b8831c8a7372fd6fcbb6f649e5315d9920cb56
visserjackson-code <jcv74@proton.me>
2026-03-22 16:54:27 -0400

    more debugging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const calculatorRoutes = require('./routes/calculatorRoutes')
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use('/', express.static('public'));
 // app.use("/mytest", testRoutes);
