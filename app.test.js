8e644faf68352f3126d50baf329072605a95d3da
visserjackson-code <jcv74@proton.me>
2026-03-24 09:09:05 -0400

    typeerror invalid URL on sample code

---
+++ app.test.js
@@ -0,0 +1,22 @@
+//import supertest and the express app
+const request = require('supertest');
+const app = require('./app');
+
+describe('Calculator Routes', () => {
+
+    //generate some random numbers to test the calculator
+    let number1 = Math.floor(Math.random() * 1_000_000);
+    let number2 = Math.floor(Math.random() * 1_000_000);
+
+    test('GET /calculator/add => sum of numbers', () => {
+        const response = request(app)
+
+            .get(`calculator/add?/num1=${number1}&num2=${number2}`)
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(response.body).toEqual({
+            result: (number1 + number2)
+        });
+    });
+
+});
--- public/js/square.test.js
+++
@@ -1,5 +0,0 @@
-const { square } = require('./square');
-
-test('square 5 to get 25', () => {
-    expect(square(5)).toBe(25);
-});
